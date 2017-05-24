abe2cfffe09a405a872ba8af090da263d7aa508c
Saikal <saikalk@Saikals-MacBook-Pro.local>
2017-05-24 10:58:22 -0700

    done isSubsetOf

--- isSubsetOf/isSubsetOf.js
+++ isSubsetOf/isSubsetOf.js
@@ -23,4 +23,57 @@
 
 Array.prototype.isSubsetOf = function (arr) {
   // your code here
+  let result;
+  for (let i = 0; i < this.length; i++){
+    result = false;
+    for (let k = 0; k < arr.length; k++){
+      if(this[i] === arr[k]){
+        result = true;
+        break;
+      }
+    }
+    if(result === false){
+      return false;
+    }
+  }
+  return true;
 };
+
+
+
+//EXTRA CREDIT:
+
+// Array.prototype.isSubsetOf = function (array) {
+//   // your code here
+//   for(let m = 0; m < this.length; m++){
+//     for (let n = 0; n < array.length; n++){
+//     ifHasCheck(this[m], array[n])
+//     }
+//   }
+//   var ifHasCheck = function(thisArr, arr){
+//     let result;
+//     for (let i = 0; i < thisArr.length; i++){
+//       result = false;
+//       for (let k = 0; k < arr.length; k++){
+//         if(thisArr[i] === arr[k]){
+//           result = true;
+//           break;
+//         }
+//       }
+//       if(result === false){
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// };
+
+//  var a = ['commit','push', 'push']
+//  console.log(a.isSubsetOf(['commit','rebase','blame', 'push'])); // true
+
+
+
+
+
+
+
