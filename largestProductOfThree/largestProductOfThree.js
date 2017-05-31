854f289858e3b1186522c3798e8c36daae1deb49
Saikal <saikalk@Saikals-MacBook-Pro.local>
2017-05-31 09:52:23 -0700

    largestProductofThree is done

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -8,6 +8,28 @@
  */
 
 
-var largestProductOfThree = function(array) {
-  // TODO: everything
+
+
+var largestProductOfThree = function(arr) {
+  arr.sort();
+  var result = false;
+  var count = 1;
+  var myProduct = arr[arr.length-1];
+  while (result === false && count < 10){
+    result = true;
+    myProduct= myProduct*count;
+    for(var i = 0; i < arr.length; i++){
+      if(myProduct%arr[i] !== 0){
+        result = false;
+        break;
+      }
+    }
+    
+    if(result !== false){
+      result = true;
+    }
+    count++;
+  }
+
+  return myProduct;
 };
