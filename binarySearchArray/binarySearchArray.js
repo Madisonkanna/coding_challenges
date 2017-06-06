1314da84b2fb58679bb9f5689fd49e175cfb1f5c
Saikal <saikalk@Saikals-MacBook-Pro.local>
2017-06-06 09:57:34 -0700

    finished but not working

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -8,6 +8,21 @@
  * console.log(index); // 3
  */
 
-var binarySearch = function (array, target) {
+var binarySearch = function (arr, target) {
+
+  if(target === arr[arr.length/2]){
+
+    return arr.length/2;
+  } 
+  if(arr.length/2 === 0 && target !== arr[arr.length/2]){
+    return "Not Found";
+  }
+
+  if(target>arr[arr.length/2]){
+      binarySearch(arr.slice(arr[arr.length/2+1], arr[arr.length-1]));
+    } else if(target<arr[arr.length/2]){
+
+      binarySearch(arr.slice(arr[0], arr[arr.length/2-1]));
+    }
 };
 
