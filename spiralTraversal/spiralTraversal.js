276ac3cbbefdc9fb4160bebab7b8907992e9aaa4
Saikal <saikalk@Saikals-MacBook-Pro.local>
2017-07-05 09:11:22 -0700

    did not do

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -18,7 +18,8 @@ var spiralTraversal = function(matrix) {
 
   // TODO: Implement me!
   var result = [];
-  var n = matrix.length-1;
+  var n = matrix[0].length-1;
+  var j = matrix.length-1;
   for(var m = 0; m < matrix.length; m++){
     if(n>=0){
       var i = m;
@@ -27,26 +28,27 @@ var spiralTraversal = function(matrix) {
         //result.pop();
       }
       k=n;
-      for(i = m+1; i < n; i++){
+      for(i = m+1; i < j+1; i++){      //fixed till length of the matrix
         result.push(matrix[i][k]);
       }
-      i = n;
-      for(k=n;k>=m;k--){
-        result.push(matrix[i][k]);
+      i = j;
+      for(k=n-1;k>=m;k--){
+        result.push(matrix[i][k]);     //fix k=n-1 i=j
       }
 
       k=m;
-      for(i=n-1;i>=m+1;i--){
-        result.push(matrix[i][k]);
+      for(i=j-1;i>=m+1;i--){
+        result.push(matrix[i][k]);    //fix i=j-1
         //result.pop();
 
       }
       n--;
+      j--;
     }
   }
-  if(matrix.length%2===1){
-    result.pop();
-  }
+  // if(matrix.length%2===1){
+  //   result.pop();
+  // }
   return result;
 };
 
@@ -73,6 +75,28 @@ console.log(spiralTraversal([
         ]));
 
 
+console.log("*******last test: ", spiralTraversal([
+      [1,2,3,4],
+      [5,6,7,8],
+      [9,10,11,12],
+      [13,14,15,16],
+      [17,18,19,20],
+      [21,22,23,24]
+    ]));
 
+console.log("******* one more test: ", spiralTraversal([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [10,11,12]
+  ]));
 
+console.log("******* two more test: ", spiralTraversal([
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16],
+    [17,18,19,20]
+  ]));
 
+console.log("******* three more test: ", spiralTraversal([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18], [19, 20, 21], [22, 23, 24]]));
