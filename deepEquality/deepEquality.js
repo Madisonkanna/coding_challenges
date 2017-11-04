c7cba823b5a8c3984088c9546ddf005dcdfc9208
Saikal <skalmanbetova@gmail.com>
2017-11-04 12:34:47 -0700

    Update deepEquality.js

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -12,26 +12,51 @@
   *
   */
 
-var deepEquals = function(apple, orange) {
-  var arr1 = [];
-  var arr2 = [];
+//MY SOLUTION!!!
+// var deepEquals = function(apple, orange) {
+//   var arr1 = [];
+//   var arr2 = [];
 
-  var arr = [];
-  var toArray = function(obj){
-    for(var key in obj){
-      if((Object.prototype.toString.call(obj[key]) !== '[object Object]')){
-        if(arr[(arr.length)-1].length === 1){
-          arr[arr.length-1].push([key, obj[key]]);
-        }else arr.push([key, obj[key]]);
-      }
-      if((Object.prototype.toString.call(obj[key]) === '[object Object]')){
-        // arr.push([key, obj[key]])
-        arr.push([key]);
-        toArray(obj[key]);
-      //return toArray(obj[key]);
-      }
-    }
-    return arr;
-  }
+//   var arr = [];
+//   var toArray = function(obj){
+//     for(var key in obj){
+//       if((Object.prototype.toString.call(obj[key]) !== '[object Object]')){
+//         if(arr[(arr.length)-1].length === 1){
+//           arr[arr.length-1].push([key, obj[key]]);
+//         }else arr.push([key, obj[key]]);
+//       }
+//       if((Object.prototype.toString.call(obj[key]) === '[object Object]')){
+//         // arr.push([key, obj[key]])
+//         arr.push([key]);
+//         toArray(obj[key]);
+//       //return toArray(obj[key]);
+//       }
+//     }
+//     return arr;
+//   }
   
-};
+// };
+
+
+
+var deepEquals = function(apple, orange) {
+
+  if (apple === orange) { return true; }
+  if (apple && !orange || !apple && orange) { return false; }
+  if (!(apple instanceof Object) || !(orange instanceof Object)) { return false; }
+  var appleKeys = Object.keys(apple);
+  var orangeKeys = Object.keys(orange);
+  if (appleKeys.length !== orangeKeys.length) { return false; }
+  if (appleKeys.length === 0) { return true; } // two empty objects are equal
+  for (var i = 0; i < appleKeys.length; i++) {
+    if (!deepEquals(apple[appleKeys[i]], orange[appleKeys[i]])) { return false; }
+  }
+  return true;
+ };
+
+
+
+
+
+
+
